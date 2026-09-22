da47fd0bacaa828f79dff3fca93748cfa19fa2c5
Nick-theballer1967 <nick@ecoai.energy>
2026-09-22 21:22:24 +0100

    Fix HubSpot chat overlap on mobile

--- site.js
+++ site.js
@@ -35,4 +35,59 @@
       section_name: 'contact_form'
     });
   });
+
+  var hubspotWidgetObserver;
+  var observedHubSpotWidget;
+
+  function observeHubSpotWidget(widget) {
+    if (!('MutationObserver' in window) || observedHubSpotWidget === widget) {
+      return;
+    }
+
+    if (hubspotWidgetObserver) {
+      hubspotWidgetObserver.disconnect();
+    }
+
+    observedHubSpotWidget = widget;
+    hubspotWidgetObserver = new MutationObserver(updateHubSpotClearance);
+    hubspotWidgetObserver.observe(widget, {
+      attributes: true,
+      attributeFilter: ['class', 'style']
+    });
+  }
+
+  function updateHubSpotClearance() {
+    var widget = document.getElementById('hubspot-messages-iframe-container');
+
+    if (!widget) {
+      if (hubspotWidgetObserver) {
+        hubspotWidgetObserver.disconnect();
+        hubspotWidgetObserver = null;
+      }
+
+      observedHubSpotWidget = null;
+      document.body.classList.remove('has-hubspot-widget');
+      document.documentElement.style.removeProperty('--hubspot-mobile-clearance');
+      return;
+    }
+
+    observeHubSpotWidget(widget);
+
+    var height = Math.ceil(widget.getBoundingClientRect().height);
+    document.body.classList.add('has-hubspot-widget');
+    document.documentElement.style.setProperty('--hubspot-mobile-clearance', (height + 8) + 'px');
+  }
+
+  if ('MutationObserver' in window) {
+    var hubspotBodyObserver = new MutationObserver(updateHubSpotClearance);
+    hubspotBodyObserver.observe(document.body, {
+      childList: true,
+      subtree: false
+    });
+  }
+
+  window.addEventListener('load', updateHubSpotClearance);
+  window.addEventListener('resize', updateHubSpotClearance);
+  window.setTimeout(updateHubSpotClearance, 1500);
+  window.setTimeout(updateHubSpotClearance, 5000);
 })();
